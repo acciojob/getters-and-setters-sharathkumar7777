99f72197f263eb1d92492382dbd6a179621ba528
Sharath Naik <117922641+sharathkumar7777@users.noreply.github.com>
2025-12-24 13:33:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-//complete this code
-class Person {}
+class Person {
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
 
-class Student extends Person {}
+    get name() {
+        return this._name;
+    }
 
-class Teacher extends Person {}
+    set name(newName) {
+        this._name = newName;
+    }
 
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+    get age() {
+        return this._age;
+    }
+
+    set age(newAge) {
+        this._age = newAge;
+    }
+}
+
+class Student extends Person {
+    constructor(name, age) {
+        super(name, age);
+    }
+
+    study() {
+        console.log(`${this.name} is studying`);
+    }
+}
+
+class Teacher extends Person {
+    constructor(name, age) {
+        super(name, age);
+    }
+
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
+}
